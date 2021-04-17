baa2f8a0ed63cab9bbe02eb8157748457d4a00f9
MKTeja <f20180532@hyderabad.bits-pilani.ac.in>
2021-04-17 16:16:28 +0530

    123

---
+++ app/src/main/java/com/example/creativecart/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.example.creativecart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button CreateAccountButton;
+    private EditText InputName, InputEmailID, InputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        CreateAccountButton = (Button) findViewById(R.id.register_btn);
+        InputName = (EditText) findViewById(R.id.register_username_input);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+        InputEmailID = (EditText) findViewById(R.id.register_Email_ID_input);
+    }
+}
